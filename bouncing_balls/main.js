3dbfe0e934879dbbf9b15fed053adfb34ea547db
ygn <947498533@qq.com>
2018-08-10 16:46:00 +0800

    增加撞击侦察,小球撞击后颜色改变

--- bouncing_balls/main.js
+++ bouncing_balls/main.js
@@ -50,6 +50,20 @@ Ball.prototype.update = function () {
     this.y += this.velY;
 };
 
+Ball.prototype.collisionDetect = function () {
+    for (var j = 0; j < balls.length; j++) {
+        if (!(this === balls[j])) {
+            var dx = this.x - balls[j].x;
+            var dy = this.y - balls[j].y;
+            var distance = Math.sqrt(dx * dx + dy * dy);
+
+            if (distance < this.size + balls[j].size) {
+                balls[j].color = this.color = 'rgb(' + random(0, 255) + ',' + random(0, 255) + ',' + random(0, 255) + ')';
+            }
+        }
+    }
+};
+
 var balls = [];
 
 function loop() {
@@ -71,6 +85,7 @@ function loop() {
     for (var i = 0; i < balls.length; i++) {
         balls[i].draw();
         balls[i].update();
+        balls[i].collisionDetect();
     }
 
     requestAnimationFrame(loop);
