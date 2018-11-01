ae6b2817342ec794691325f325e7b6ba5331501c
ygn <947498533@qq.com>
2018-11-01 14:03:29 +0800

    python条件判断和页面输入；使用input函数得到用户输入的内容是str类型，str类型不能和整数直接比较，必须先使用int（）函数转换成整数

--- python/first.py
+++ python/first.py
@@ -1,6 +1,17 @@
 print('hello world!')
 print(100+200+300)
-age = 20
-if age > 18:
+age = 12
+if age >= 18:
     print('你是一个成年人！你的年龄是：',age)
+elif age >= 6:
+    print('teenager!')
+else:
+    print('kid!!')
+
+birth=input('birth:')
+birth=int(birth)
+if birth < 2000:
+    print('00后')
+else:
+    print('00前!')
     
