a6e7cf436c616fdd871f32fb46efc475f1cda124
ygn <947498533@qq.com>
2018-11-07 10:41:03 +0800

    python基础语法

--- python/first.py
+++ python/first.py
@@ -1,17 +1,45 @@
-print('hello world!')
-print(100+200+300)
-age = 12
-if age >= 18:
-    print('你是一个成年人！你的年龄是：',age)
-elif age >= 6:
-    print('teenager!')
-else:
-    print('kid!!')
+# print('hello world!')
+# print(100+200+300)
 
-birth=input('birth:')
-birth=int(birth)
-if birth < 2000:
-    print('00后')
-else:
-    print('00前!')
-    
+#=========== 条件判断 ============
+# age = 12
+# if age >= 18:
+#     print('你是一个成年人！你的年龄是：',age)
+# elif age >= 6:
+#     print('teenager!')
+# else:
+#     print('kid!!')
+
+#=========== 页面输入  =========== 
+# birth=input('birth:')
+# birth=int(birth)
+# if birth < 2000:
+#     print('00后')
+# else:
+#     print('00前!')
+
+# height = 1.75
+# weight = 80.5
+# BMI = weight/pow(height,2)
+# if BMI < 18.5:
+#     print('过轻')
+# else:
+#     print('肥胖')
+
+#===========  循环  =========== 
+# names = ['james','wade','ross']
+# for name in names:
+#     print(name)
+
+# sum = 0 
+# for x in range(100):
+#     sum += x
+# print(sum)
+
+# L = ['Bart', 'Lisa', 'Adam']
+# for name in L:
+#     print('Hello!',name)
+
+#============   dict和set   ==============
+d = {'james':28,'wade':24,'iversion':18}
+print(d['james'])
