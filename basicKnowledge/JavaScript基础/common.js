b7a8a3518cabc85205a12686d27e2e98ec681d96
ygn <947498533@qq.com>
2019-02-02 20:51:10 +0800

    封装获取文本内容代码

--- basicKnowledge/JavaScript基础/common.js
+++ basicKnowledge/JavaScript基础/common.js
@@ -1,3 +1,11 @@
 function $ (id) {
     return document.getElementById(id)
 };
+
+function getInnerText (element) {
+    if (typeof element.innerText == 'undefind') {
+        return element.textContent;
+    }else{
+        return element.innerText;
+    };
+};
