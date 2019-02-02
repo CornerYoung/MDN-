4a9d435b66997127b12e234f420c3427140328f0
ygn <947498533@qq.com>
2019-02-02 20:58:03 +0800

    封装设置任意标签中任意文本内容兼容代码

--- basicKnowledge/JavaScript基础/common.js
+++ basicKnowledge/JavaScript基础/common.js
@@ -2,10 +2,21 @@ function $ (id) {
     return document.getElementById(id)
 };
 
+//封装获取任意标签中任意文本内容代码
 function getInnerText (element) {
-    if (typeof element.innerText == 'undefind') {
+    if (typeof element.innerText == 'undefined') {
         return element.textContent;
     }else{
         return element.innerText;
+    };
+};
+
+//封装设置任意标签中任意文本内容代码
+function setInnerText (element,text) {
+    //判断浏览器是否支持 innerText
+    if (typeof element.textContent == 'undefined') {
+        element.innerText = text;
+    }else{
+        element.textContent = text;
     };
 };
