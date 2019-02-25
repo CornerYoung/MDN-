db64fc6f3f490c5c7287ac791ba3b40352cc9624
ygn <947498533@qq.com>
2019-02-25 22:52:59 +0800

    使用node中fs模块读取data文件夹中的文件，失败显示error，成功显示hello.txt内容

--- Node学习/01/0002_读取文件.js
+++ Node学习/01/0002_读取文件.js
@@ -13,5 +13,23 @@ var fs = require('fs');
 //readFile是fs中的一个方法，传两个参数，一个是文件路径，一个是回调函数
 
 fs.readFile ('./data/hello.txt',function(error,data){
-    console.log(data.toString())
+//        成功
+//          data 数据
+//          error null
+//        失败
+//          data undefined没有数据
+//          error 错误对象
+
+/**
+ * <Buffer 68 65 6c 6c 6f 20 6e 6f 64 65 6a 73 0d 0a>
+ * 文件中存储的其实都是二进制数据 0 1
+ * 这里为什么看到的不是 0 和 1 呢？原因是二进制转为 16 进制了
+ * 但是无论是二进制01还是16进制，人类都不认识
+ * 所以我们可以通过 toString 方法把其转为我们能认识的字符
+ */
+    if(error){
+        console.log('文件读取失败！');
+    }else{
+        console.log(data.toString())
+    };
 })
