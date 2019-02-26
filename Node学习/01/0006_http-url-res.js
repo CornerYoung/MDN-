3b684927166a08857dd638bc3a2c25d28d43b15b
ygn <947498533@qq.com>
2019-02-26 21:20:04 +0800

    🉑根据url地址的不同，接收不同的数据

--- Node学习/01/0006_http-url-res.js
+++ Node学习/01/0006_http-url-res.js
@@ -2,23 +2,36 @@ var http = require('http');
 
 var server = http.createServer();
 
-server.on('request',function(request,response){
+server.on('request', function (request, response) {
     var url = request.url;
-    
-    console.log('收到客户端请求，请求路径是： '+request.url);
 
-    if (url == '/favicon.ico'){
-        response.write('hello NodeJs!');
-        response.end();
-    }else if(url == '/login'){
-        response.write('login NodeJs!');
-        response.end();
-    }else{
-        response.write('你好吗？How are you?');
-        response.end();
+    console.log('收到客户端请求，请求路径是： ' + url);
+
+    // if (url == '/favicon.ico'){
+    //     response.write('hello NodeJs!');
+    //     response.end();
+    // }else if(url == '/login'){
+    //     response.write('login NodeJs!');
+    //     response.end();
+    // }else{
+    //     response.write('你好吗？How are you?');
+    //     response.end();
+    // };
+
+    if(url === '/products'){
+        var products = [
+            {'苹果 X':11800},
+            { '菠萝 X': 1800},
+            {'Vivo':2000}
+        ];
     };
+
+    //throw new TypeError('First argument must be a string or Buffer');
+    //必须传入 字符串类型 或者 二进制
+    response.end(JSON.stringify(products));
+
 });
 
-server.listen(3000,function(){
+server.listen(3000, function () {
     console.log('服务器启动成功了！可以通过http://127.0.0.1:3000进行访问！');
 });
