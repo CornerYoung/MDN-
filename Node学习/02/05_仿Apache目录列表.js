69a45e5e4a6b6b6c99815cd6a15da5b15ad03f47
ygn <947498533@qq.com>
2019-02-28 22:26:07 +0800

    fs.readdir读取到文件目录，获得文件夹数组，遍历，填充table，替代模版文件中的标记

--- Node学习/02/05_仿Apache目录列表.js
+++ Node学习/02/05_仿Apache目录列表.js
@@ -1,19 +1,41 @@
 var http = require('http');
 var fs = require('fs');
-
 var server = http.createServer();
+var wwwDir = '/Users/yangguoning/ygn-study/jquery';
 
 server.on('request', function (req, res) {
     var url = req.url;
 
-    if (url === '/') {
-        fs.readFile('./template.html', function (error, data) {
-            if (error) {
-                return res.end('404 Not Found!');
+    fs.readFile('./template.html', function (error, data) {
+        if (error) {
+            return res.end('404 Not Found!');
+        };
+
+        //fs模块读取文件夹的方法
+        fs.readdir(wwwDir, function (err, files) {
+            if (err) {
+                return res.end('Can not wwwDir!');
             };
+            var content = '';
+            files.forEach(function (item) {
+                //使用es6语法拼接字符串，${}用来引用变量
+                content += `
+                    <tr>
+                        <td data-value="apple/"><a class="icon dir" href="/Users/yangguoning/ygn-study/jquery/${item}">${item}/</a></td>
+                        <td class="detailsColumn" data-value="0"></td>
+                        <td class="detailsColumn" data-value="1509589967">2017/11/2 上午10:32:47</td>
+                    </tr>
+                `
+            });
+
+            //将二进制data数据进行字符串化处理
+            data = data.toString();
+            //把模版中的 @_@ 用拼接好的 content 替代
+            data = data.replace('@_@', content);
             res.end(data)
         });
-    };
+
+    });
 
 });
 
