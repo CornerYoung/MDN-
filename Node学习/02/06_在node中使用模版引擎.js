cfe6f9573d4970a7cc94e0fe6953195ced77da6c
ygn <947498533@qq.com>
2019-02-28 23:41:13 +0800

    使用fs核心模块读取art-template.html文件，给模版引擎使用|模版引擎需要接收字符串，接收到的数据要进行字符串处理

--- Node学习/02/06_在node中使用模版引擎.js
+++ Node学习/02/06_在node中使用模版引擎.js
@@ -1,7 +1,17 @@
 var template = require('art-template');
+var fs = require('fs');
+var tmp = fs.readFile('./art-template.html',function(err,data){
+    if(err){
+        return console.log('读取失败！');
+    };
 
-var ret = template.render('hello {{name}}',{
-    name : 'Jack!'
+    data = data.toString();
+
+    var ret = template.render(data, {
+        name: 'Young',
+        age:18,
+        player:'Lebron James GOAT'
+    });
+    console.log(ret)
 });
 
-console.log(ret);
