507988dcff956a00e0c341dc89313ba796b368ea
ygn <947498533@qq.com>
2019-03-02 10:35:54 +0800

    表单提交的请求路径，其中具有用户动态填写的内容，所以不能通过去判断完整的 url 路径来处理这个请求

--- Node学习/02/feedback/app.js
+++ Node学习/02/feedback/app.js
@@ -1,5 +1,6 @@
 var http = require('http');
 var fs = require('fs');
+var url = require('url');
 var template = require('art-template');
 
 var contents = [{
@@ -24,10 +25,18 @@ var contents = [{
     dateTime: '2019-03-01'
 }];
 
+// http://127.0.0.1:3000/comment?name=令狐冲&message=独孤九剑、吸星大法、易筋经%0D%0A
+// 表单提交的请求路径，其中具有用户动态填写的内容，所以不能通过去判断完整的 url 路径来处理这个请求
+// 结论：只需要判定，如果请求路径是 /pinglun 的时候，那么就认为用户提交表单的请求过来了
+
 http
     .createServer(function (req, res) {
-        var url = req.url;
-        if (url === '/') {
+        // 使用 url.parse 方法将路径解析为一个方便操作的对象，第二个参数为 true 表示直接将查询字符串转为一个对象（通过 query 属性来访问）
+        var parseObj = url.parse(req.url, true);
+        // 单独获取不包含查询字符串的路径部分（该路径不包含 ? 之后的内容）
+        var pathName = parseObj.pathname;
+        //var url = req.url;
+        if (pathName === '/') {
             fs.readFile('./views/index.html', function (err, data) {
                 if (err) {
                     return res.end(err);
@@ -39,14 +48,14 @@ http
 
                 res.end(htmlStr);
             });
-        } else if (url.indexOf('/post') === 0) {
+        } else if (pathName.indexOf('/post') === 0) {
             fs.readFile('./views/post.html', function (err, data) {
                 if (err) {
                     return res.end('404 Not Found!');
                 };
                 res.end(data);
             });
-        } else if (url.indexOf('/public/') === 0) {
+        } else if (pathName.indexOf('/public/') === 0) {
             //在 index.html 页面中需要访问很多外链资源，如果不设置 url 将造成页面阻塞
             //所以把需要访问的静态资源都统一放到 public 文件夹中，当访问的 url 以 /public/ 开头时，就能访问到这些静态资源了
             //public 整个目录中的资源都允许被访问
@@ -57,12 +66,23 @@ http
              *  /public/js/main.js
              *  /public/lib/
              */
-            fs.readFile('.' + url, function (err, data) {
+            fs.readFile('.' + pathName, function (err, data) {
                 if (err) {
                     return res.end('404 Not Found!');
                 };
                 res.end(data);
             });
+        }else if(pathName === '/comment'){
+            // 注意：此时无论 /pinglun? 之后是什么内容，都可以访问到这个路径，因为 pathname 是不包含 ? 之后的那个路径
+            // 一次请求对应一次响应，响应结束这次请求也就结束了
+            res.end(JSON.stringify(parseObj.query));
+            // 我们已经使用 url 模块的 parse 方法把请求路径中的查询字符串给解析成一个对象了
+            // 所以接下来要做的就是：
+            //    1. 获取表单提交的数据 parseObj.query
+            //    2. 将当前时间日期添加到数据对象中，然后存储到数组中
+            //    3. 让用户重定向跳转到首页 /
+            //       当用户重新请求 / 的时候，我数组中的数据已经发生变化了，所以用户看到的页面也就变了
+            
         } else {
             //其他 url 设置成 404
             fs.readFile('./views/404.html', function (err, data) {
