7e9f964b8c10754fdf916bf74609ea81b528ce74
ygn <947498533@qq.com>
2019-03-05 21:13:02 +0800

    express可以自动通过判断页面需要渲染的内容格式设置content-type

--- Node学习/02/express-demo/app.js
+++ Node学习/02/express-demo/app.js
@@ -13,6 +13,29 @@ app.get('/about',function(req,res){
     res.send('hello express!!');
 });
 
+app.get('/login',function(req,res){
+    res.send(`
+        < !DOCTYPE html >
+        <html lang="en">
+            <head>
+                <meta charset="UTF-8">
+                <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                <meta http-equiv="X-UA-Compatible" content="ie=edge">
+                <title>art-template模版引擎</title>
+            </head>
+                <body>
+                    <p>大家好！我叫{{ name }}</p>
+                    <p>我今年{{ age }}岁</p>
+                    <p>我喜欢的球星是{{ player }}！</p>
+                </body>
+                <script>
+                    alert('html');
+                </script>
+        </html>
+    `
+    );
+});
+
 //底层 server.listen
 app.listen(3000,function(){
     console.log('app is running port:3000');
