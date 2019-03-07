31c25e8a7684a0f19216d0b47fda820469e18e43
ygn <947498533@qq.com>
2019-03-07 22:46:12 +0800

    node管理系统静态页面搭建|配置好favicon.ico图标和样式文件

--- Node学习/03/code/admin-node/app.js
+++ Node学习/03/code/admin-node/app.js
@@ -1,8 +1,18 @@
 var express = require('express');
+
 var app = express();
+app.engine('html',require('express-art-template'));
+app.use('/node_modules', express.static('./node_modules'));
+app.use('/public', express.static('./public'));
 
 app.get('/',function(req,res){
-    res.send('hello world!');
+    res.render('index.html',{
+        player:[
+            '詹姆斯',
+            '韦德',
+            '鲍尔'
+        ]
+    });
 });
 
 app.listen(3000,function(){
