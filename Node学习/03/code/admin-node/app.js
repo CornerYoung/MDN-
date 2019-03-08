e7d42496fe78a8b42c72db6b4af45d30cc41b399
ygn <947498533@qq.com>
2019-03-08 22:45:09 +0800

    从文件中读取到的数据一定是字符串，所以这里一定要手动转换成对象

--- Node学习/03/code/admin-node/app.js
+++ Node学习/03/code/admin-node/app.js
@@ -7,6 +7,8 @@ app.use('/node_modules', express.static('./node_modules'));
 app.use('/public', express.static('./public'));
 
 app.get('/', function (req, res) {
+    //readFile 的第二个参数是可选的，传入 utf8 可以使读取到的文件按照 utf8 编码进行字符转换
+    //除了这样来转换，也可以通过 data.toString（）的方式
     fs.readFile('./db.json', 'utf8', function (err, data) {
         if (err) {
             return res.status(500).send('Server error.');
