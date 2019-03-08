22dc8604b99223dfebf36f7fd15c26f22c87ab02
ygn <947498533@qq.com>
2019-03-08 13:02:43 +0800

    读取db.json数据文件，渲染到页面中去，readFile 的第二个参数是可选的，传入 utf8 可以使读取到的文件按照 utf8 编码进行字符转换

--- Node学习/03/code/admin-node/app.js
+++ Node学习/03/code/admin-node/app.js
@@ -1,20 +1,24 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express();
-app.engine('html',require('express-art-template'));
+app.engine('html', require('express-art-template'));
 app.use('/node_modules', express.static('./node_modules'));
 app.use('/public', express.static('./public'));
 
-app.get('/',function(req,res){
-    res.render('index.html',{
-        player:[
-            '詹姆斯',
-            '韦德',
-            '鲍尔'
-        ]
+app.get('/', function (req, res) {
+    fs.readFile('./db.json', 'utf8', function (err, data) {
+        if (err) {
+            return res.status(500).send('Server error.');
+        };
+        res.render('index.html', {
+            //从文件中读取到的数据一定是字符串，所以这里一定要手动转换成对象
+            players: JSON.parse(data).players,
+            firuts: ['香蕉', '西瓜', '苹果']
+        });
     });
 });
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log('Running... 3000')
 });
