498c7c7493f5e9bd1ca89e92b241a9e47989e61c
ygn <947498533@qq.com>
2019-03-08 23:06:12 +0800

    app.js作为项目的入口文件，要求具有固定性，不能随意更改入口文件，router.js路由函数被暴露出来，app.js进行接收

--- Node学习/03/code/admin-node/app.js
+++ Node学习/03/code/admin-node/app.js
@@ -1,11 +1,13 @@
 var express = require('express');
-var fs = require('fs');
+var router = require('./router');
 
 var app = express();
 
 app.engine('html', require('express-art-template'));
 app.use('/node_modules', express.static('./node_modules'));
 app.use('/public', express.static('./public'));
+
+router(app);
 
 app.listen(3000, function () {
     console.log('Running... 3000')
