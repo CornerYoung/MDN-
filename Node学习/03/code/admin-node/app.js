b6c3482c51fd858522be49a24d880f435280dca8
ygn <947498533@qq.com>
2019-03-09 12:40:37 +0800

    清除router.js文件中封装的函数，将路由容器挂载到 app 服务中去（express提供）

--- Node学习/03/code/admin-node/app.js
+++ Node学习/03/code/admin-node/app.js
@@ -7,7 +7,9 @@ app.engine('html', require('express-art-template'));
 app.use('/node_modules', express.static('./node_modules'));
 app.use('/public', express.static('./public'));
 
-router(app);
+//把路由容器挂载到 app 服务中去
+app.use(router);
+//router(app);
 
 app.listen(3000, function () {
     console.log('Running... 3000')
