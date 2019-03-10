287ce31dde83ab31cb11cae1278930ba5d8462d1
ygn <947498533@qq.com>
2019-03-10 22:57:18 +0800

    开发删除球员信息功能|【bug】对形参 id 的数据格式进行转换，否则会出现bug，使得 deletePlayerId 始终等于 -1

--- Node学习/03/code/admin-node/players.js
+++ Node学习/03/code/admin-node/players.js
@@ -130,7 +130,7 @@ exports.updateById = function (player, callback) {
             //这里如果写成了一个等于号将出现bug
             return item.id === player.id;
         });
-        
+
         for (var key in player) {
             plaRet[key] = player[key]
         };
@@ -153,4 +153,33 @@ exports.updateById = function (player, callback) {
 /**
  * 删除球员
  */
-exports.delete = function () {};
+exports.deleteById = function (id, callback) {
+    fs.readFile(dbPath, 'utf8', function (err, data) {
+        if (err) {
+            callback(err);
+        };
+
+        //将获取到的数据转化为对象格式，得到其中的 players
+        var players = JSON.parse(data).players;
+
+        //遍历 players，如果需要删除的元素的 id 与其中的 id 匹配，就拿到这个元素
+        var deletePlayerId = players.findIndex(function (item) {
+            //这里要对形参 id 的数据格式进行转换，否则会出现bug，使得 deletePlayerId 始终等于 -1
+            return item.id === parseInt(id);
+        });
+        
+        //根据 id，在 players 数组中删除这一项元素
+        players.splice(deletePlayerId, 1);
+
+        //将删除操作完成之后的 players 数组转化为字符串重新写入文件
+        var fileData = JSON.stringify({
+            players: players
+        });
+        fs.writeFile(dbPath, fileData, function (err) {
+            if (err) {
+                callback(err);
+            };
+            callback(null);
+        });
+    });
+};
