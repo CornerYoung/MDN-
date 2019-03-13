42e8082281b5392f19845afd5288eed4d27e131a
ygn <947498533@qq.com>
2019-03-13 22:01:22 +0800

    es5默认参数的方法，容错判断

--- basicKnowledge/ECMAScript6/03_变量的解构赋值/01_解构赋值.js
+++ basicKnowledge/ECMAScript6/03_变量的解构赋值/01_解构赋值.js
@@ -14,6 +14,15 @@ console.log(name, age);
  * 函数参数的默认值
  * 传递的参数也是要以对象的形式
  */
+function func(type, num, isHappy) {
+    type = type || 'click';
+    num = num || 123;
+    isHappy = isHappy || true;
+    console.log('type: ' + type, 'num: ' + num, 'isHappy: ' + isHappy);
+};
+func();
+func('mouseover',456,false);
+
 function fn({
     type = 'click',
     num = 123,
