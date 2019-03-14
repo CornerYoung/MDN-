acf717f45b6eab1b867075208d1da65897d697cc
ygn <947498533@qq.com>
2019-03-14 21:31:39 +0800

    可以保证回调函数的执行顺序，必须使用这种嵌套的方式

--- Node学习/04/review/04-callback-hell.js
+++ Node学习/04/review/04-callback-hell.js
@@ -1,5 +1,33 @@
 var fs = require('fs')
 
+/**
+ * 无法保证回调顺序，速度由文件大小决定
+ */
+fs.readFile('./data/a.txt', 'utf8', function (err, data) {
+  if (err) {
+    throw err
+  }
+  console.log(data)
+});
+
+fs.readFile('./data/b.txt', 'utf8', function (err, data) {
+  if (err) {
+    throw err
+  }
+  console.log(data)
+});
+
+fs.readFile('./data/c.txt', 'utf8', function (err, data) {
+  if (err) {
+    throw err
+  }
+  console.log(data)
+});
+
+
+/**
+ * 可以保证回调函数的执行顺序，必须使用这种嵌套的方式
+ */
 fs.readFile('./data/a.txt', 'utf8', function (err, data) {
   if (err) {
     // return console.log('读取失败')
