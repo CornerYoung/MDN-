6e46916f40dcf9ef3f5081d9ec3f12111453b8c2
ygn <947498533@qq.com>
2019-03-14 09:53:22 +0800

    对象的解构赋值

--- basicKnowledge/ECMAScript6/03_变量的解构赋值/01_解构赋值.js
+++ basicKnowledge/ECMAScript6/03_变量的解构赋值/01_解构赋值.js
@@ -1,3 +1,19 @@
+/**
+ * 数组的解构赋值
+ */
+//只要等号两边的模式相同，左边的变量就会被赋予对应的值
+let [a, b, c] = [1, 2, 3];
+let [, , third] = ["first", "second", "third"];
+let [head, ...tail] = [1, 2, 3, 4];
+let [x, y, ...z] = ['a'];
+//解构赋值允许指定默认值。
+let [x, y = 'b'] = ['a']; // x='a', y='b'
+let [x, y = 'b'] = ['a', undefined]; // x='a', y='b'
+
+
+/**
+ * 对象的解构赋值
+ */
 let obj = {
     name: 'Jack',
     age: 18
@@ -9,6 +25,22 @@ let {
 } = obj;
 
 console.log(name, age);
+
+//对象的解构也可以指定默认值。
+var { x = 3 } = {};
+x // 3
+
+var { x, y = 5 } = { x: 1 };
+x // 1
+y // 5
+
+var { x: y = 3 } = {};
+y // 3
+
+var { x: y = 3 } = { x: 5 };
+y // 5
+
+var { message: msg = 'Something went wrong' } = {};
 
 /**
  * 函数参数的默认值
