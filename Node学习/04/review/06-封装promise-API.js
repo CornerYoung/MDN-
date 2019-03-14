14caa00e5a8fa0343939ddf285727c96665e99cb
ygn <947498533@qq.com>
2019-03-14 22:19:43 +0800

    将读取文件路径的重复代码高度封装起来|通过promise解决了嵌套问题|所有异步操作都可以封装

--- Node学习/04/review/06-封装promise-API.js
+++ Node学习/04/review/06-封装promise-API.js
@@ -1,5 +1,6 @@
 var fs = require('fs')
 
+//将读取文件路径的重复代码高度封装起来
 function pReadFile(filePath) {
   return new Promise(function (resolve, reject) {
     fs.readFile(filePath, 'utf8', function (err, data) {
