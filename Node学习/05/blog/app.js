59ddff6935af42623cb9d90d59e53161f63eebb9
ygn <947498533@qq.com>
2019-03-16 22:19:45 +0800

    配置解析表单 POST 请求体插件（注意⚠️ 一定要在 app.use(router) 之前）

--- Node学习/05/blog/app.js
+++ Node学习/05/blog/app.js
@@ -1,7 +1,9 @@
 var express = require('express');
-var path =require('path');
+var path = require('path');
+var router = require('./routes/router');
+var bodyParser = require('body-parser');
 
-var app=express();
+var app = express();
 
 //开放资源文件夹
 //app.use('/public/',express.static('./public/'));
@@ -11,16 +13,18 @@ var app=express();
 //__filename 可以用来动态地获取当前文件的绝对路径
 //在文件操作中，使用相对路径是不可靠的，因为在 Node 中文件操作的路径被设计为相对于执行 node 命令所处的路径。（这样设计不是bug，是有使用场景的）
 //所以为了解决这个问题，需要把相对路径改为绝对路径，这里就可以使用 __dirname 或者 __filename 来解决这个问题了
-app.use('/public/', express.static(path.join(__dirname,'./public/')));
-app.use('/node_modules/',express.static(path.join(__dirname,'./node_modules/')));
+app.use('/public/', express.static(path.join(__dirname, './public/')));
+app.use('/node_modules/', express.static(path.join(__dirname, './node_modules/')));
 
-app.engine('html',require('express-art-template'));
-app.set('views',path.join(__dirname,'./views/')); //默认就是 ./views 目录，再写一次是为了以后想改的话方便修改
+app.engine('html', require('express-art-template'));
+app.set('views', path.join(__dirname, './views/')); //默认就是 ./views 目录，再写一次是为了以后想改的话方便修改
 
-app.get('/',function(req,res){
-    res.render('index.html');
-});
+//配置解析表单 POST 请求体插件（注意⚠️ 一定要在 app.use(router) 之前）
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+//把路由挂在到 app 中
+app.use(router);
 
-app.listen(8888,function(){
+app.listen(8888, function () {
     console.log('Port 8888 is Running...');
 });
