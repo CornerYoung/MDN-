85a6ceda071d5e8a69eb2cc1394fb0ee22891614
ygn <947498533@qq.com>
2019-03-18 21:56:38 +0800

    配置一个全局错误处理中间件,这样就可以把路由文件中的错误处理剥离简化

--- Node学习/05/blog/app.js
+++ Node学习/05/blog/app.js
@@ -21,7 +21,9 @@ app.engine('html', require('express-art-template'));
 app.set('views', path.join(__dirname, './views/')); //默认就是 ./views 目录，再写一次是为了以后想改的话方便修改
 
 //配置解析表单 POST 请求体插件（注意⚠️ 一定要在 app.use(router) 之前）
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 app.use(bodyParser.json());
 //配置 express-session 插件，可以通过 req.session 来访问和设置 Session 成员
 //添加 session 数据：req.session.foo = 'bar'
@@ -29,11 +31,25 @@ app.use(bodyParser.json());
 app.use(session({
     secret: 'keyboard cat', // 配置加密字符串，它会在原有加密基础上和这个字符串拼起来区加密，增加安全性
     resave: false,
-    saveUninitialized: true,
+    saveUninitialized: true //无论是否使用session，都默认分配一把钥匙
 }))
 
 //把路由挂在到 app 中
 app.use(router);
+
+//配置一个处理 404 的中间件
+app.use(function (req, res, next) {
+    res.render('404.html');
+    next();
+});
+
+//配置一个全局错误处理中间件
+app.use(function (err, req, res, next) {
+    res.status(500).json({
+        err_code: 500,
+        message: err.message
+    });
+});
 
 app.listen(8888, function () {
     console.log('Port 8888 is Running...');
