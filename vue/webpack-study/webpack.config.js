4325a0070f1a6519fbafa47d5dae3ff26be6d636
ygn <947498533@qq.com>
2019-03-27 21:51:05 +0800

    配置 dev-server 命令参数的第二种形式，相对来说，这种方式麻烦一些

--- vue/webpack-study/webpack.config.js
+++ vue/webpack-study/webpack.config.js
@@ -1,5 +1,7 @@
 const path = require('path');
 
+const webpack = require('webpack'); // 启用热更新的 第2步
+
 // 这个配置文件，其实就是一个 JS 文件，通过 Node 中的模块操作，向外暴露了一个 配置对象
 module.exports = {
     // 入口，表示，要使用 webpack 打包哪个文件
@@ -9,7 +11,17 @@ module.exports = {
         path: path.join(__dirname, './dist'),
         // 这是指定 输出的文件的名称
         filename: 'bundle.js'
-    }
+    },
+    devServer: { // 这是配置 dev-server 命令参数的第二种形式，相对来说，这种方式麻烦一些
+        //  --open --port 3000 --contentBase src --hot
+        open: true, // 自动打开浏览器
+        port: 3000, // 设置启动时候的运行端口
+        contentBase: 'src', // 指定托管的根目录
+        hot: true // 启用热更新 的 第1步
+    },
+    plugins: [ // 配置插件的节点
+        new webpack.HotModuleReplacementPlugin() // new 一个热更新的 模块对象， 这是 启用热更新的第 3 步
+    ]
 }
 
 // 当在 控制台，直接输入 webpack 命令执行的时候，webpack 做了以下几步：
