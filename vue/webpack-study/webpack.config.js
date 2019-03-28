6e91d9b257e9ce4dc8b102dd98fec566211e9725
ygn <947498533@qq.com>
2019-03-28 22:52:23 +0800

    plugins:配置插件的节点|module:这个节点，用于配置所有第三方模块加载器,里面的rules是所有第三方模块的 匹配规则

--- vue/webpack-study/webpack.config.js
+++ vue/webpack-study/webpack.config.js
@@ -2,6 +2,13 @@ const path = require('path');
 
 const webpack = require('webpack'); // 启用热更新的 第2步
 
+// 导入在内存中生成 HTML 页面的插件
+// 只要是插件，都一定要放到 plugins 节点中去
+// 这个插件的两个作用：
+//  1. 自动在内存中根据指定页面生成一个内存的页面
+//  2. 自动，把打包好的 bundle.js 追加到页面中去
+const htmlWebpackPlugin = require('html-webpack-plugin');
+
 // 这个配置文件，其实就是一个 JS 文件，通过 Node 中的模块操作，向外暴露了一个 配置对象
 module.exports = {
     // 入口，表示，要使用 webpack 打包哪个文件
@@ -20,8 +27,47 @@ module.exports = {
         hot: true // 启用热更新 的 第1步
     },
     plugins: [ // 配置插件的节点
-        new webpack.HotModuleReplacementPlugin() // new 一个热更新的 模块对象， 这是 启用热更新的第 3 步
-    ]
+        new webpack.HotModuleReplacementPlugin(), // new 一个热更新的 模块对象， 这是 启用热更新的第 3 步
+        new htmlWebpackPlugin({ // 创建一个在内存中 生成 HTML 页面的插件
+            // 指定模板页面，将来会根据指定的页面路径，去生成内存中的页面
+            template: path.join(__dirname, './src/index.html'),
+            // 指定生成的页面的名称
+            filename: 'index.html'
+        })
+    ],
+    module: { // 这个节点，用于配置所有第三方模块加载器 
+        rules: [ // 所有第三方模块的 匹配规则
+
+            { //  配置处理 .css 文件的第三方loader 规则
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            { //配置处理 .less 文件的第三方 loader 规则
+                test: /\.less$/,
+                use: ['style-loader', 'css-loader', 'less-loader']
+            },
+            { // 配置处理 .scss 文件的 第三方 loader 规则
+                test: /\.scss$/,
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            },
+            { // 处理图片路径
+                // limit 给定的值，是图片的大小，单位是 byte，如果我们引用的图片，大于或等于给定的 limit值，
+                //则不会被转为base64格式的字符串，如果图片小于给定的 limit 值，则会被转为 base64的字符串
+                //name 属性可以设置图片被编译好之后的名称
+                test: /\.(jpg|png|jpeg|gif|bmp)$/,
+                use: ['url-loader?limit=300&name=[hash:8]-[name].[ext]']
+            },
+            { //处理字体文件
+                test: /\.(eot|ttf|svg|woff|woff2)$/,
+                use: 'url-loader'
+            },
+            { // 配置 Babel 来转换高级的ES语法
+                test: /\.js$/,
+                use: 'babel-loader',
+                exclude: /node_modules/
+            }
+        ]
+    }
 }
 
 // 当在 控制台，直接输入 webpack 命令执行的时候，webpack 做了以下几步：
