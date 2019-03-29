10153e86dccc7d88e384768a13a26244d664042c
ygn <947498533@qq.com>
2019-03-29 21:12:50 +0800

    在webpack.config.js中为vue完整的引用路径设置好了别名，这里就可以直接使用写法from “vue”了

--- vue/webpack-vue/src/main.js
+++ vue/webpack-vue/src/main.js
@@ -10,8 +10,8 @@
 console.log('ok');
 // 在webpack 中尝试使用 Vue：
 // 注意： 在 webpack 中， 使用 import Vue from 'vue' 导入的 Vue 构造函数，功能不完整，只提供了 runtime-only 的方式，并没有提供 像网页中那样的使用方式；
-//import Vue from 'vue';
-import Vue from '../node_modules/vue/dist/vue.js';
+import Vue from 'vue';
+//import Vue from '../node_modules/vue/dist/vue.js';
 // 包的查找规则：
 // 1. 找 项目根目录中有没有 node_modules 的文件夹
 // 2. 在 node_modules 中 根据包名，找对应的 vue 文件夹
