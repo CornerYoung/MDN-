0e625de9ca0918ec40a996ccfa86739dad8d79f3
ygn <947498533@qq.com>
2019-03-29 23:29:46 +0800

    webpack与vue-router结合使用|模块加载报错的话，删除node_modules文件夹，确保package.json版本正确后重新下载

---
+++ vue/webpack-vue-router/src/main/Account.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <h2>这是Account组件</h2>
+    </div>
+</template>
+
+<script>
+export default {
+  name: "account"
+};
+</script>
+
+<style>
+h2{
+    font-family: Cambria, Cochin, Georgia, Times, 'Times New Roman', serif;
+    color: cyan;
+}
+</style>
+
