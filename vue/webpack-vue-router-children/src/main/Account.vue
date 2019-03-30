e8a6a2ae156f84582a083a04bbb0191a3290e775
ygn <947498533@qq.com>
2019-03-30 08:19:14 +0800

    普通的 style 标签只支持 普通的 样式，如果想要启用 scss 或 less ，需要为 style 元素，设置 lang 属性|只要style 标签， 是在 .vue 组件中定义的，那么，推荐都为 style 开启 scoped 属性

--- vue/webpack-vue-router-children/src/main/Account.vue
+++ vue/webpack-vue-router-children/src/main/Account.vue
@@ -13,10 +13,15 @@ export default {
 };
 </script>
 
-<style>
-h2 {
+<style lang="scss">
+/* 普通的 style 标签只支持 普通的 样式，如果想要启用 scss 或 less ，需要为 style 元素，设置 lang 属性 */
+// 只要style 标签， 是在 .vue 组件中定义的，那么，推荐都为 style 开启 scoped 属性
+div{
   font-family: Cambria, Cochin, Georgia, Times, "Times New Roman", serif;
   color: cyan;
+  a{
+    text-decoration: none;
+  }
 }
 </style>
 
