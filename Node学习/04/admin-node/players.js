5081042b1d3342752e7b8645a2b28c5bdcaaca38
ygn <947498533@qq.com>
2019-04-02 20:27:58 +0800

    错信息提示：当前的URL字符串分析器已弃用，将在将来的版本中删除。要使用新的解析器

--- Node学习/04/admin-node/players.js
+++ Node学习/04/admin-node/players.js
@@ -3,9 +3,14 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const dbPath = './db.json';
 
-mongoose.connect('mongodb://localhost/test', {
-    useMongoClient: true
-});
+//报错信息提示：当前的URL字符串分析器已弃用，将在将来的版本中删除。要使用新的解析器，请将选项useNewUrlParser:true传递到mongoclient.connect。
+//DeprecationWarning: current URL string parser is deprecated, and will be removed in a future version. 
+//To use the new parser, pass option { useNewUrlParser: true } to MongoClient.connect.
+// mongoose.connect('mongodb://localhost/test', {
+//     useMongoClient: true
+// });
+mongoose.connect('mongodb://localhost/test', { useNewUrlParser: true });
+
 const players = new Schema({
     name: {
         type: String,
