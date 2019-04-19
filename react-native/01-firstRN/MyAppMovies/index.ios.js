94076f7014baa9ded7d1f8f0f1293ac2e66c8e7a
ygn <947498533@qq.com>
2019-04-19 21:44:15 +0800

    拉取真正的数据

--- react-native/01-firstRN/MyAppMovies/index.ios.js
+++ react-native/01-firstRN/MyAppMovies/index.ios.js
@@ -22,6 +22,16 @@ const MOCKED_MOVIES_DATA = [
 const REQUEST_URL = "https://raw.githubusercontent.com/facebook/react-native/0.51-stable/docs/MoviesExample.json";
 
 export default class MyAppMovies extends Component {
+  constructor(props) {
+    super(props);   //这一句不能省略，照抄即可
+    this.state = {
+      movies: null,  //这里放你自己定义的state变量及初始值
+    };
+    // 在ES6中，如果在自定义的函数里使用了this关键字，则需要对其进行“绑定”操作，否则this的指向不对
+    // 像下面这行代码一样，在constructor中使用bind是其中一种做法（还有一些其他做法，如使用箭头函数等）
+    this.fetchData = this.fetchData.bind(this);
+  };
+  
   render() {
     var movie = MOCKED_MOVIES_DATA[0];
     return (
