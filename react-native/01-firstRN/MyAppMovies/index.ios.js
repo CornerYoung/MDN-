be247e9ce71c3bdf0778e6453d23e6b5876b68f0
ygn <947498533@qq.com>
2019-04-19 21:20:04 +0800

    修改缩略图、标题样式，flex布局

--- react-native/01-firstRN/MyAppMovies/index.ios.js
+++ react-native/01-firstRN/MyAppMovies/index.ios.js
@@ -25,9 +25,11 @@ export default class MyAppMovies extends Component {
     var movie = MOCKED_MOVIES_DATA[0];
     return (
       <View style={styles.container}>
-        <Text>{movie.title}</Text>
-        <Text>{movie.year}</Text>
-        <Image source={{ uri: movie.posters.thumbnail }} style={styles.thumbnail}/>
+        <Image source={{ uri: movie.posters.thumbnail }} style={styles.thumbnail} />
+        <View style={styles.rightContainer}>
+          <Text style={styles.title}>{movie.title}</Text>
+          <Text style={styles.year}>{movie.year}</Text>
+        </View>
       </View>
     );
   }
@@ -36,12 +38,24 @@ export default class MyAppMovies extends Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    flexDirection: 'row',
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#F5FCFF"
   },
+  rightContainer: {
+    flex: 1,
+  },
+  title: {
+    fontSize: 20,
+    marginBottom: 8,
+    textAlign: 'center',
+  },
+  year: {
+    textAlign: 'center',
+  },
   thumbnail: {
-    width: 53,
+    width: 253,
     height: 81
   }
 });
