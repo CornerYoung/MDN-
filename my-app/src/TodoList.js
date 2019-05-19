fa9fb6c54eedbe7adec38fb428efd24577c14599
ygn <947498533@qq.com>
2019-05-19 16:32:20 +0800

    抽离出TodoItem子组件，将数组内容和函数操作通过props传递给子组件

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import TodoItem from './TodoItem';
 
 class TodoList extends Component {
 
@@ -26,10 +27,19 @@ class TodoList extends Component {
                     {
                         this.state.list.map((item, index) => {
                             return (
-                                <li 
-                                    key={index} 
-                                    onClick={this.handleItemDelete.bind(this,index)}
-                                >{item}</li>
+                                <div>
+                                    <TodoItem 
+                                        content={item} 
+                                        index={index}
+                                        deleteItem={this.handleItemDelete.bind(this)}
+                                    />
+                                    {
+                                        // <li 
+                                        //     key={index} 
+                                        //     onClick={this.handleItemDelete.bind(this,index)}
+                                        // >{item}</li>
+                                    }
+                                </div>
                             )
                         })
                     }
