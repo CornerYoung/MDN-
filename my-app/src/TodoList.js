a5654dd2ed0d5368b0cc9d24d39797517cbb3bfc
ygn <947498533@qq.com>
2019-05-19 17:16:20 +0800

    TodoList父组件的代码优化，es6和逻辑拆分|key值必须写在循环的最外层上，为了书写注释，循环子组件外边包了一个div，所以key值写在了div上

--- my-app/src/TodoList.js
+++ my-app/src/TodoList.js
@@ -23,51 +23,67 @@ class TodoList extends Component {
                         onClick={this.handleBtnClick.bind(this)}
                     >提交</button>
                 </div>
-                <ul>
-                    {
-                        this.state.list.map((item, index) => {
-                            return (
-                                <div>
-                                    <TodoItem 
-                                        content={item} 
-                                        index={index}
-                                        deleteItem={this.handleItemDelete.bind(this)}
-                                    />
-                                    {
-                                        // <li 
-                                        //     key={index} 
-                                        //     onClick={this.handleItemDelete.bind(this,index)}
-                                        // >{item}</li>
-                                    }
-                                </div>
-                            )
-                        })
-                    }
-                </ul>
+                <ul>{this.getTodoItem()}</ul>
             </Fragment>
         )
     }
 
-    handleInputChange(e) {
-        this.setState({
-            inputValue: e.target.value
+    getTodoItem() {
+        return this.state.list.map((item, index) => {
+            return (
+                <div key={index}>
+                    <TodoItem
+                        content={item}
+                        index={index}
+                        deleteItem={this.handleItemDelete.bind(this)}
+                    />
+                    {
+                        //key值要写在循环的最外层上
+                        // <li 
+                        //     key={index} 
+                        //     onClick={this.handleItemDelete.bind(this,index)}
+                        // >{item}</li>
+                    }
+                </div>
+            )
         })
     }
 
+    handleInputChange(e) {
+        // this.setState({
+        //     inputValue: e.target.value
+        // })
+        //使用es6语法进行代码优化，箭头函数返回对象的话不用return，直接用一个括号包起来
+        //需要把e.target.value赋值给value，否则会报错
+        const value = e.target.value;
+        this.setState(()=>({
+            inputValue: value
+        }))
+    }
+
     handleBtnClick() {
-        this.setState({
-            list: [...this.state.list, this.state.inputValue],
+        // this.setState({
+        //     list: [...this.state.list, this.state.inputValue],
+        //     inputValue: ''
+        // })
+        this.setState((prevState)=>({
+            list: [...prevState.list, prevState.inputValue],
             inputValue: ''
-        })
+        }))
     }
 
     handleItemDelete(index) {
         // immutable state不允许我们做任何的改变，不要直接去修改state里面的内容，对React性能优化有问题
-        console.log(index)
-        const newList = [...this.state.list];
-        newList.splice(index,1);
-        this.setState({
-            list: newList
+        // console.log(index)
+        // const newList = [...this.state.list];
+        // newList.splice(index,1);
+        // this.setState({
+        //     list: newList
+        // })
+        this.setState((prevState)=>{
+            const list = [...prevState.list];
+            list.splice(index, 1);
+            return {list}
         })
     }
 };
