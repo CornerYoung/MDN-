0521c4dd2100ad10c7c10b62f608646390085824
ygn <947498533@qq.com>
2019-05-19 11:41:20 +0800

    项目暂时不需要脚手架生成的App组件，使用TodoList作为React演示Demo

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 
+// import { Component } from 'react'; es6解构赋值
+// // 等价于
+// import React from 'react';
+// const Component = React.Component;
+
 class App extends Component {
   render() {
     return (
