723467b12df5ee41f9bd4db2714c97c430defa3a
ygn <947498533@qq.com>
2019-05-19 10:18:00 +0800

    简化工程代码，去除不必要的文件和代码

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,13 +3,8 @@ import React, { Component } from 'react';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <h2>Hello React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+          Hello React
       </div>
     );
   }
