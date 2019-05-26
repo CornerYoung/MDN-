06ed3d901d79e7d5223abe81a5ed1b8988bf9de7
CornerYoung <947498533@qq.com>
2019-05-26 19:49:34 +0800

    扮演记事本的角色，通过一系列数据逻辑处理，更新state。注意⚠️reducer 可以接收state，但是绝不能修改state，所以要将state深拷贝给newState

--- my-app/src/store/reducer.js
+++ my-app/src/store/reducer.js
@@ -1,8 +1,20 @@
 const defaultState = {
-    inputValue:'12354',
+    inputValue:'reducer',
     list:[1,2]
 };
 
+//reducer 可以接收state，但是绝不能修改state
 export default (state=defaultState,action) => {
+    if (action.type === 'change_input_value'){
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.inputValue = action.value;
+        return newState;
+    };
+    if (action.type === 'add_todo_item'){
+        const newState = JSON.parse(JSON.stringify(state));
+        newState.list.push(newState.inputValue);
+        newState.inputValue = '';
+        return newState;
+    }
     return state;
 };
