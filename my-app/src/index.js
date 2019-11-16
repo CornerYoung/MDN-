52b637d2978903e6045101f9c667b74fdaca511f
Young <947498533@qq.com>
2019-11-16 14:25:43 +0800

    增值税整体结构

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-//import App from './App';
-import TodoList from './TodoList';
+import App from './App';
+//import TodoList from './TodoList';
 //all in js
+import './style/index.less';
 
 ReactDOM.render(
-  //<App />,
-  <TodoList />,
+  <App />,
+  //<TodoList />,
   document.getElementById('root')
 );
