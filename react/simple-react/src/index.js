6478cc10e79541654303e319f2d2ab592f87f3b9
Young <947498533@qq.com>
2019-11-19 19:00:29 +0800

    构建一个App构造函数，包括render方法，render方法返回一个element

--- react/simple-react/src/index.js
+++ react/simple-react/src/index.js
@@ -5,12 +5,29 @@ import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 
-let App = <div><h1>welcome!react</h1><h2>hello!world!!</h2></div>;
+//let App = <div><h1>welcome!react</h1><h2>hello!world!!</h2></div>;
+//react rend方法探索，第一个参数传一个element，这个element可以赋值给一个变量，当作render的第一个参数（注意element不是字符串，如果有多个，需要使用div包裹起来）
+class App{
+    constructor(name,age){
+        this.name = name;
+        this.age = age;
+    }
+
+    say(){
+        return this.name+':'+this.age;
+    };
+    render(){
+        return (
+            <div><h1>welcome!react</h1><h2>hello!world!!</h2></div>
+        );
+    };
+};
 // ReactDOM.render(<App />, document.getElementById('root'));
 // ReactDOM.render( <h1>welcome!react</h1> , document.getElementById('root'));
-ReactDOM.render(App, document.getElementById('root'));
+//ReactDOM.render(App, document.getElementById('root'));
 
-
+console.log(new App().render());
+ReactDOM.render(new App().render(), document.getElementById('root'));
 
 
 
