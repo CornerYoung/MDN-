0a30a6843e6b7107c0b71d5817d53d33ec5b1e81
CornerYoung <947498533@qq.com>
2020-01-03 15:22:44 +0800

    前端小组分享jsx语法原理

--- react/simple-react/src/index.js
+++ react/simple-react/src/index.js
@@ -1,41 +1,44 @@
 import React,{Component} from 'react'
 import {render} from 'react-dom'
 
+// class App extends Component {
+//     render() {
+//         console.log(this)
+//         return (
+//             <div className="app">
+//                 <h1>hello world!</h1>
+//                 <p>{this.props.name}</p>
+//             </div>
+//         )
+//     }
+// } 
+
 class App extends Component {
     render() {
         return (
             React.createElement(
                 'div',
                 {
-                    className: 'app',
-                    id: 'app'
+                    className:'app'
                 },
                 React.createElement(
                     'h1',
-                    {
-                        className: 'title'
-                    },
-                    'Hello World!!'
+                    null,
+                    'hello world'
                 ),
                 React.createElement(
                     'p',
                     null,
-                    'React is good!'
+                    this.props.name
                 )
             )
         )
     }
 }
 
-render(<App />, document.querySelector('#root'))
-
-
-
-
-
-
-//使用类的方式创建一个组件，但这个组件使用的是JSX语法，不是合法的js语法
-//所以React在真正运行时会把JSX语法的代码编译成使用React.createElement方法的样子来运行，这样是合法的js语法
+render(
+    <App name="React"/>
+    , document.querySelector('#root'))
 
 
 
